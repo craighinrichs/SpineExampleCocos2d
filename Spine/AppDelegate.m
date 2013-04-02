521d17c71784221422212948a6edb44687ba4572
Craig Hinrichs <craig.hinrichs@gmail.com>
2013-04-01 23:34:05 -0500

    Initial

--- Spine/AppDelegate.m
+++ Spine/AppDelegate.m
@@ -9,7 +9,7 @@
 #import "cocos2d.h"
 
 #import "AppDelegate.h"
-#import "IntroLayer.h"
+#import "ExampleLayer.h"
 
 @implementation MyNavigationController
 
@@ -47,7 +47,7 @@
 	if(director.runningScene == nil) {
 		// Add the first scene to the stack. The director will draw it immediately into the framebuffer. (Animation is started automatically when the view is displayed.)
 		// and add the scene to the stack. The director will run it when it automatically when the view is displayed.
-		[director runWithScene: [IntroLayer scene]];
+		[director runWithScene: [ExampleLayer scene]];
 	}
 }
 @end
