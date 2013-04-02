699cde353f2a1b2a0dd9aa474bee8cc84b15aa9d
Craig Hinrichs <craig.hinrichs@gmail.com>
2013-04-01 23:23:56 -0500

    Initial Commit

---
+++ Spine/AppDelegate.h
@@ -0,0 +1,28 @@
+//
+//  AppDelegate.h
+//  Spine
+//
+//  Created by Craig Hinrichs on 4/1/13.
+//  Copyright Craig Hinrichs 2013. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "cocos2d.h"
+
+// Added only for iOS 6 support
+@interface MyNavigationController : UINavigationController <CCDirectorDelegate>
+@end
+
+@interface AppController : NSObject <UIApplicationDelegate>
+{
+	UIWindow *window_;
+	MyNavigationController *navController_;
+
+	CCDirectorIOS	*director_;							// weak ref
+}
+
+@property (nonatomic, retain) UIWindow *window;
+@property (readonly) MyNavigationController *navController;
+@property (readonly) CCDirectorIOS *director;
+
+@end
---
+++ Spine/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Spine
+//
+//  Created by Craig Hinrichs on 4/1/13.
+//  Copyright Craig Hinrichs 2013. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"AppController");
+    [pool release];
+    return retVal;
+}
